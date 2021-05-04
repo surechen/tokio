1ef57904efe656370177dfb775c1507ca57c4dd0
github-action-benchmark <github@users.noreply.github.com>
2021-05-04 02:56:23 +0000

    add rt_multi_threaded (cargo) benchmark result for 14bb2f624f1ac3d440dd302219a48e9e8e926b50

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1612340014124,
+  "lastUpdate": 1620096983031,
   "repoUrl": "https://github.com/surechen/tokio",
   "entries": {
     "sync_rwlock": [
@@ -1837,6 +1837,54 @@ window.BENCHMARK_DATA = {
             "name": "yield_many",
             "value": 19835493,
             "range": "± 2924522",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "russell.r.cohen@gmail.com",
+            "name": "Russell Cohen",
+            "username": "rcoh"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "14bb2f624f1ac3d440dd302219a48e9e8e926b50",
+          "message": "io: add write_all_buf to AsyncWriteExt (#3737)",
+          "timestamp": "2021-05-01T20:54:38+02:00",
+          "tree_id": "43242f7e908bb52a3a7fd59d83e191839b661ece",
+          "url": "https://github.com/surechen/tokio/commit/14bb2f624f1ac3d440dd302219a48e9e8e926b50"
+        },
+        "date": 1620096980316,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "chained_spawn",
+            "value": 215062,
+            "range": "± 36617",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "ping_pong",
+            "value": 769353,
+            "range": "± 122981",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "spawn_many",
+            "value": 5635825,
+            "range": "± 1144983",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "yield_many",
+            "value": 23332417,
+            "range": "± 4350116",
             "unit": "ns/iter"
           }
         ]
